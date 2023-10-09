478cf239d62379d11f57a63dae60630e3ce356e1
maria ryabikovskaya <niceshizachan@gmail.com>
2023-10-09 15:34:22 +0300

    feat: add create test method in test controller

--- server/controllers/testController.js
+++ server/controllers/testController.js
@@ -1,18 +1,37 @@
-const {Test} = require('../models/models');
-const ApiError = require('../error/ApiError');
-
+const { Test, Answer, Question } = require("../models/models");
+const ApiError = require("../error/ApiError");
 
 class TestController {
   async create(req, res) {
-    const {role_name} = req.body
-    const role = await Test.create({role_name})
-    return res.json(role)
+    try {
+      const { test_name, test_desc, psychotype, test_content } = req.body;
+
+      const test = await Test.create({ test_name, test_desc, psychotype });
+      const testId = test.test_id;
+
+      for (const item of test_content) {
+        const { question, answers } = item;
+        const questionData = await Question.create({
+          test_id: testId,
+          question_text: question,
+        });
+        const questionId = questionData.question_id;
+
+        for (const answer of answers) {
+          await Answer.create({ question_id: questionId, answer_text: answer });
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+    return res;
   }
 
   async getall(req, res) {
-      const roles = await Test.findAll()
-      return res.json(roles)
+    const tests = await Test.findAll();
+    return res.json(tests);
   }
 }
 
-module.exports = new TestController()
+module.exports = new TestController();
