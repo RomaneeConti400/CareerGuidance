4d0a18e3a531e67a4a5a9b5e3a9fe6324cf06de1
maria ryabikovskaya <niceshizachan@gmail.com>
2023-10-28 23:10:05 +0300

    feat: add "get by test id" (& user id) method in testUser

--- server/controllers/testUserController.js
+++ server/controllers/testUserController.js
@@ -1,19 +1,36 @@
-import {TestUser} from '../models/models.js'
-import ApiError from '../error/ApiError.js'
+import { TestUser, User } from "../models/models.js";
+import ApiError from "../error/ApiError.js";
 import { generatePublicId } from "../utils/public_id.js";
 
 class TestUserController {
   async create(req, res) {
-    const {user_id, test_id, result_value} = req.body
-    const tesus_id = generatePublicId()
-    const role = await TestUser.create({tesus_id, user_id, test_id, result_value})
-    return res.json(role)
+    const { user_id, test_id, result_value } = req.body;
+    const tesus_id = generatePublicId();
+    const result = await TestUser.create({
+      tesus_id,
+      user_id,
+      test_id,
+      result_value,
+    });
+    return res.json(result);
   }
 
   async getall(req, res) {
-      const roles = await TestUser.findAll()
-      return res.json(roles)
+    const results = await TestUser.findAll();
+    return res.json(results);
+  }
+
+  async getbytestid(req, res) {
+    const test_id = req.params.id;
+    const user_id = req.user.user_id;
+    const result = await TestUser.findOne({
+      where: {
+        test_id,
+        user_id,
+      },
+    });
+    return res.json(result);
   }
 }
 
-export default new TestUserController()
+export default new TestUserController();
--- server/routes/testUserRouter.js
+++ server/routes/testUserRouter.js
@@ -1,8 +1,10 @@
-import Router from 'express'
-const router = new Router()
-import testUserController from '../controllers/testUserController.js'
+import Router from "express";
+const router = new Router();
+import authMiddleware from "../middleware/authMiddleware.js"
+import testUserController from "../controllers/testUserController.js";
 
-router.post('/create', testUserController.create)
-router.get('/getall', testUserController.getall)
+router.post("/create", testUserController.create);
+router.get("/getall", testUserController.getall);
+router.get("/getbytestid/:id",authMiddleware, testUserController.getbytestid);
 
-export default router
+export default router;
