31c83edac1ae2d6d265010bf6ca9305dce3b74d5
maria ryabikovskaya <niceshizachan@gmail.com>
2023-10-29 18:12:09 +0300

    refactor: format code

--- server/routes/testRouter.js
+++ server/routes/testRouter.js
@@ -1,5 +1,6 @@
 import Router from "express";
 const router = new Router();
+import authMiddleware from "../middleware/authMiddleware.js";
 import testController from "../controllers/testController.js";
 
 router.post("/create", testController.create);
@@ -7,6 +8,6 @@ router.get("/getall", testController.getall);
 router.get("/getbyid/:id", testController.getbyid);
 router.get("/gettestcontentbyid/:id", testController.gettestcontentbyid);
 router.get("/fill", testController.fill);
-router.post("/postuserresult", testController.postuserresult);
+router.post("/postuserresult", authMiddleware, testController.postuserresult);
 
 export default router;
