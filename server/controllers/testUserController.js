4f5a795c447f5069c5095d926069090f05df9b9b
maria ryabikovskaya <niceshizachan@gmail.com>
2023-10-29 18:12:42 +0300

    feat: modify testUser create method

--- server/controllers/testUserController.js
+++ server/controllers/testUserController.js
@@ -1,18 +1,32 @@
-import { TestUser, User } from "../models/models.js";
+import { TestUser } from "../models/models.js";
 import ApiError from "../error/ApiError.js";
 import { generatePublicId } from "../utils/public_id.js";
 
 class TestUserController {
   async create(req, res) {
-    const { user_id, test_id, result_value } = req.body;
     const tesus_id = generatePublicId();
-    const result = await TestUser.create({
-      tesus_id,
-      user_id,
-      test_id,
-      result_value,
+    const { test_id, user_id, result_value } = req.body;
+    const testResult = await TestUser.findOne({
+      where: {
+        test_id,
+        user_id,
+      },
     });
-    return res.json(result);
+    if (!testResult) {
+      let result = await TestUser.create({
+        tesus_id: tesus_id,
+        user_id: user_id,
+        test_id: test_id,
+        result_value: result_value,
+      });
+      result = await TestUser.findOne({
+        where: {
+          test_id,
+          user_id,
+        },
+      });
+      return res.json(result);
+    }
   }
 
   async getall(req, res) {
--- server/routes/testUserRouter.js
+++ server/routes/testUserRouter.js
@@ -1,10 +1,10 @@
 import Router from "express";
 const router = new Router();
-import authMiddleware from "../middleware/authMiddleware.js"
+import authMiddleware from "../middleware/authMiddleware.js";
 import testUserController from "../controllers/testUserController.js";
 
 router.post("/create", testUserController.create);
 router.get("/getall", testUserController.getall);
-router.get("/getbytestid/:id",authMiddleware, testUserController.getbytestid);
+router.get("/getbytestid/:id", authMiddleware, testUserController.getbytestid);
 
 export default router;
